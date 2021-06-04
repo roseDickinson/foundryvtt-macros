20054ce879c8901dc8bdeb4f3d10c90f0cc6b54d
roseDickinson <rose.d.dickinson@gmail.com>
2021-06-04 19:04:04 +0100

    feat: Update shadowBlade macro to time out after 10 rounds

--- shadowBlade.js
+++ shadowBlade.js
@@ -1,48 +1,66 @@
-function castAtLevel(lvl) {
-  let damage = new Roll(`${lvl}d8`).evaluate();
-  let dmgTerms = damage.terms[0].results
-  let dmgCalc = ''
-  for (let [index, term] of dmgTerms.entries()) {
-    if (index === dmgTerms.length - 1) {
-      dmgCalc += `${term.result}`
-    } else {
-      dmgCalc += `${term.result} + `
+//DAE Item Macro Execute, Effect Value = @attributes.spelldc
+
+const lastArg = args[args.length - 1];
+let tactor;
+if (lastArg.tokenId) tactor = canvas.tokens.get(lastArg.tokenId).actor;
+else tactor = game.actors.get(lastArg.actorId);
+const DAEItem = lastArg.efData.flags.dae.itemData
+
+/**
+ * Create Shadow Blade item in inventory
+ */
+if (args[0] === "on") {
+  const sl = args[1]
+  console.log("lastArg", lastArg)
+  let startRound = game.combats.combats[0].current.round
+  lastArg.efData.duration.rounds = 10
+  lastArg.efData.duration.seconds = null
+  lastArg.efData.duration.startTime = null
+  lastArg.efData.duration.startRound = startRound
+  lastArg.efData.disabled = true
+  let damage = 5
+  if (sl === "2") {
+    damage = 2
+  } else if (sl === "3" || sl === "4") {
+    damage = 3
+  } else if (sl === "5" || sl === "6") {
+    damage = 4
+  }
+  tactor.createOwnedItem(
+    {
+      "name": "Shadow Blade repeating",
+      "type": "weapon",
+      "data": {
+        "source": "Casting Shadow Blade",
+        "ability": "dex",
+        "actionType": "mwak",
+        "attackBonus": 0,
+        "equipped": true,
+        "proficient": true,
+        "weaponType": "simpleM",
+        "damage": {
+          "parts": [
+            [
+              `${damage}d10 + @mod`,
+              "psychic"
+            ]
+          ],
+        },
+        "formula": "",
+        "preparation": {
+          "mode": "prepared",
+          "prepared": false
+        },
+      },
+      "img": DAEItem.img,
     }
-  }
-  let message = `<h2> Shadow Blade </h2><br/><p>${lvl}d8(${dmgCalc}) = </p></br><h3>${damage.total} psychic damage</h3>`;
-  let chatData = {
-    user: game.user._id,
-    content: message,
-  }
-  ChatMessage.create(chatData, {})
+  );
 }
 
-new Dialog({
-  title: 'What level spell was used to create the blade?',
-  buttons: {
-    two: {
-      label: 'two',
-      callback: () => {
-        castAtLevel(2)
-      }
-    },
-    threeFour: {
-      label: 'three or four',
-      callback: () => {
-        castAtLevel(3)
-      }
-    },
-    fiveSix: {
-      label: 'five or six',
-      callback: () => {
-        castAtLevel(4)
-      }
-    },
-    sevenUp: {
-      label: 'seven or higher',
-      callback: () => {
-        castAtLevel(5)
-      }
-    },
-  }
-}).render(true)
+console.log("SHADOW BLADE - ", args[0])
+
+// Delete Shadow Blade
+if (args[0] === "off") {
+  let casterItem = tactor.data.items.find(i => i.name === "Shadow Blade repeating" && i.type === "weapon")
+  tactor.deleteOwnedItem(casterItem._id)
+}
