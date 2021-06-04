6f33e5aaf9060526681fd92d20b4e8a24b1e2003
roseDickinson <rose.d.dickinson@gmail.com>
2021-06-04 23:00:45 +0100

    feat: Improve trapAttack to properly send chat message

--- trapAttack.js
+++ trapAttack.js
@@ -1,9 +1,31 @@
+function createChatMessage(message) {
+  let chatData = {
+    user: game.user._id,
+    content: message,
+  }
+  ChatMessage.create(chatData, {})
+}
+
 game.togglePause(true, true)
+FurnacePlaylistQoL.PlaySound("Test", "drums");
 LMRTFYRoller.requestSavingThrows(token.actor, "dex")
+if (!token) {
+  return createChatMessage("TrapMacro: No token selected.")
+}
+
 let trapActor = game.actors.entities.find(a => a.name === args[0])
-if (!trapActor) return `/Whisper GM "DoTrap: Target token ${args[0]} not found"`
+if (!trapActor) {
+  return createChatMessage(`TrapMacro: Target token ${args[0]} not found`)
+}
+
 let item = trapActor.items.find(i => i.name === args[1])
-if (!item) return `/Whisper GM "DoTrap: Item ${args[1]} not found"`
+if (!item) {
+  return createChatMessage(`TrapMacro: Item ${args[1]} not found`)
+}
+
 let trapToken = canvas.tokens.placeables.find(t => t.name === args[2])
-if (!trapToken) return `/Whisper GM "DoTrap: Trap ${args[2]} not found"`
+if (!trapToken) {
+  return createChatMessage(`TrapMacro: Trap ${args[2]} not found`)
+}
+
 let damage = (await item.rollDamage({ event: { shiftKey: true } })).total
